5fc615007d4cb9f9d177459b28c73adfbac86380
kievtechnoshop@gmail.com <swr83947>
2019-11-21 00:03:36 +0200

    2

---
+++ build/precache-manifest.be83944d9e1b122824428b17abcb43a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e63d5a7290099d425c54c2561b8f1d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "532d5dfbabbc750adc44",
+    "url": "./static/css/main.813f3ac2.chunk.css"
+  },
+  {
+    "revision": "c8f4889ad84683f65a58",
+    "url": "./static/js/2.a43635ef.chunk.js"
+  },
+  {
+    "revision": "532d5dfbabbc750adc44",
+    "url": "./static/js/main.a8c4adf1.chunk.js"
+  },
+  {
+    "revision": "ce777ccb0e3aa886e8b5",
+    "url": "./static/js/runtime-main.a73e6f8f.js"
+  }
+]);
